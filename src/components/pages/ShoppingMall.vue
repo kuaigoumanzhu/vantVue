c839e69e90f08c2c2fcc6f7c1326cb778c2c6049
hanpengfei <307788836@qq.com>
2018-05-06 23:37:35 +0800

    首页搜索布局

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <van-row>
-      <van-col span="8">8</van-col>
-      <van-col span="8">8</van-col>
-      <van-col span="8">8</van-col>
-    </van-row>
+    <div class="search-bar">
+      <van-row>
+        <van-col span="3">
+          <img :src="loctionIcon" alt="" width="80%" class="localtion-icon">
+        </van-col>
+        <van-col span="16">
+          <input type="text" class="search-input">
+        </van-col>
+        <van-col span="5">
+          <van-button size="mini">查找</van-button>
+        </van-col>
+      </van-row>
+    </div>
   </div>
 </template>
 
@@ -12,11 +20,30 @@
   export default {
     data() {
       return {
+        loctionIcon: require('../../assets/images/loction.png')
       }
-    },
+    }
   }
 </script>
 
 <style scoped>
-
+  .search-bar {
+    height: 2.2rem;
+    background-color: #e5017d;
+    line-height: 2.2rem;
+  }
+  .search-input{
+    width: 100%;
+    height: 1.3rem;
+    border-top: 0px;
+    border-left: 0px;
+    border-right: 0px;
+    border-bottom: 1px solid #fff !important;
+    background-color: #e5017d;
+    color: #fff;
+  }
+  .localtion-icon{
+    padding-top:.2rem;
+    padding-left: .3rem;
+  }
 </style>
