23f759bf54c1181f48318178896329e403a76c72
hanpengfei <307788836@qq.com>
2018-05-10 00:06:16 +0800

    首页楼层效果制作

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -49,6 +49,27 @@
         </swiper>
       </div>
     </div>
+    <!-- floor -->
+    <div class="floor">
+      <div class="floor-anomaly">
+        <div class="floor-one">
+          <img :src="floor1_0.image" width="100%">
+        </div>
+        <div class="floor-two">
+          <div>
+            <img :src="floor1_1.image" width="100%">
+          </div>
+          <div>
+            <img :src="floor1_2.image" width="100%">
+          </div>
+        </div>       
+      </div>
+      <div class="floor-rule">
+        <div v-for="(item ,index) in floor1.slice(3)" :key="index">
+          <img :src="item.image" width="100%"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -67,7 +88,11 @@ import 'swiper/dist/css/swiper.css'
         reCommendGoods: [],
         swiperOption: {
           slidesPerView: 3
-        }
+        },
+        floor1: [],
+        floor1_0: {},
+        floor1_1: {},
+        floor1_2: {},
       }
     },
     components: {
@@ -84,6 +109,10 @@ import 'swiper/dist/css/swiper.css'
           this.adBanner = resp.data.data.advertesPicture //获得广告图片
           this.bannerPicArray = resp.data.data.slides   //轮播图片
           this.reCommendGoods = resp.data.data.recommend //推荐商品
+          this.floor1 = resp.data.data.floor1
+          this.floor1_0 =this.floor1[0]
+          this.floor1_1 = this.floor1[1]
+          this.floor1_2 = this.floor1[2]
         }
       }).catch(error => {
         console.log(error)
@@ -156,5 +185,39 @@ import 'swiper/dist/css/swiper.css'
     border-right: 1px solid #eee;
     font-size: 12px;
     text-align: center;
+  }
+  .floor-anomaly{
+    display: flex;
+    flex-direction:row;
+    background-color: #fff;
+    border-bottom: 1px solid #ddd; 
+  }
+  .floor-anomaly div{
+    width: 10rem;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+  }
+  .floor-one{
+    border-right: 1px solid #ddd;
+  }
+  .floor-two{
+    border-bottom: 1px solid #ddd;
+  }
+  .floor-rule{
+      display: flex;
+      flex-direction: row;
+      /* 自动换行 */
+      flex-wrap:wrap;
+      background-color: #fff;
+ 
+  }
+  .floor-rule div{
+      -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+      width:10rem;
+      border-bottom:1px solid #ddd;
+  }
+  .floor-rule div:nth-child(odd){
+      border-right: 1px solid #ddd;
   }
 </style>
---
+++ src/components/swiper/swiperDefault.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <swiper class="swiper" :options="swiperOption">
+      <swiper-slide class="swiper-slide" v-for="(item,index) in slide" :key="index">
+        slide {{item}}
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import {swiper, swiperSlide} from "vue-awesome-swiper"
+import 'swiper/dist/css/swiper.css'
+  export default {
+    data () {
+      return {
+        slide: [1,2,3,4,5,6],
+        swiperOption: {
+          // 竖屏
+          direction: 'vertical',
+          pagination: {
+            el: '.swiper-pagination'
+          }
+        }
+      }
+    },
+    components: {
+      swiper,
+      swiperSlide
+    }
+  }
+</script>
+
+<style scoped>
+  .swiper-slide{
+    height: 4rem;
+    text-align: center;
+    /* padding-top: 3rem; */
+    line-height: 4rem;
+    /* border-bottom: 1px solid #ccc; */
+  }
+  .swiper{
+    height: 7rem;
+    border-top: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+  }
+</style>
